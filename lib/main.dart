e292025586796f3be8ba9c0143d92a3e37200f3a
Celestial-GeekStarLord <cubesat007@gmail.com>
2025-11-05 22:08:17 +0545

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:camera/camera.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'pages/home_page.dart';
 import 'pages/splash_screen.dart';
 
@@ -13,6 +16,8 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Load environment variables
   await dotenv.load(fileName: ".env");
